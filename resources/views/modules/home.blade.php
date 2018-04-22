b151faae0557c56c6a27cf9e694615899f9b68e1
Mohamad Ihsan <mohamad_ihsan100@yahoo.co.id>
2018-04-22 10:30:04 +0700

    update tautan terkait

--- resources/views/modules/home.blade.php
+++ resources/views/modules/home.blade.php
@@ -69,16 +69,16 @@
     <section class="about_us_area row">
         <div class="container">
             <div class="tittle wow fadeInUp">
-                <h2>ABOUT US</h2>
-                <h4>Lorem Ipsum is simply dummy text of the printing and typesetting industry</h4>
+                <h2>DINAS PERUMAHAN DAN KAWASAN PEMUKIMAN KUTAI TIMUR</h2>
+                <h4>Beranda</h4>
             </div>
             <div class="row about_row">
                 <div class="who_we_area col-md-7 col-sm-6">
                     <div class="subtittle">
-                        <h2>WHO WE ARE</h2>
+                        <h2>SAMBUTAN</h2>
                     </div>
                     <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
-                    <a href="#" class="button_all">Contact Now</a>
+                    <!-- <a href="#" class="button_all">Contact Now</a> -->
                 </div>
                 <div class="col-md-5 col-sm-6 about_client">
                     <img src="{{ asset('assets_frontend/images/about_client.jpg') }}" alt="">
@@ -102,8 +102,8 @@
                    </div>
                    <div class="cns-content">
                         <i class="fa fa-home" aria-hidden="true"></i>
-                        <a href="#">BUILDING CONSTRUCTION</a>
-                        <p>Lorem Ipsum is simply dummy text of the print-ing and typesetting industry. Lorem Ipsum has been the industry's</p>
+                        <a href="http://lpse.kutaitimurkab.go.id/">LPSE KUTAI TIMUR</a>
+                        <p>Layanan Pengadaan Secara Elektronik (LPSE)</p><br>
                    </div>
                 </div>
                 <div class="col-md-3 col-sm-6 construction">
@@ -112,8 +112,8 @@
                    </div>
                    <div class="cns-content">
                         <i class="fa fa-keyboard-o" aria-hidden="true"></i>
-                        <a href="#">PROJECT PLANNING</a>
-                        <p>Lorem Ipsum is simply dummy text of the print-ing and typesetting industry. Lorem Ipsum has been the industry's standard dummy </p>
+                        <a href="https://www.pu.go.id/">PU-net</a>
+                        <p>Kementrian Pekerjaan Umum dan Perumahan Rakyat Republik Indonesia </p>
                    </div>
                 </div>
                 <div class="col-md-3 col-sm-6 construction">
@@ -122,8 +122,8 @@
                    </div>
                    <div class="cns-content">
                         <i class="fa fa-gavel" aria-hidden="true"></i>
-                        <a href="#">HOUSE RENOVATION</a>
-                        <p>Lorem Ipsum is simply dummy text of the print-ing and typesetting industry. Lorem Ipsum has been the industry's standard dummy </p>
+                        <a href="https://sirup.lkpp.go.id/sirup">SIRUP</a>
+                        <p>Lembaga Kebijakan Pengadaan Barang/Jasa Pemerintah</p><br>
                    </div>
                 </div>
                 <div class="col-md-3 col-sm-6 construction">
@@ -132,8 +132,8 @@
                    </div>
                    <div class="cns-content">
                         <i class="fa fa-home" aria-hidden="true"></i>
-                        <a href="#">BUILDING CONSTRUCTION</a>
-                        <p>Lorem Ipsum is simply dummy text of the print-ing and typesetting industry. Lorem Ipsum has been the industry's</p>
+                        <a href="http://www.kutaitimurkab.go.id/">PEMERINTAH KUTIM</a>
+                        <p>Link Pemerintahan Kutai Timur Kalimantan Timur</p><br>
                    </div>
                 </div>
             </div>
