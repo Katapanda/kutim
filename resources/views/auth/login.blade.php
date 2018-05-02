111ae297134ffe3aeb08182299f024241a6ab2e9
Irfan Rangga Gumilar <ranggacuwaps@gmail.com>
2018-05-02 21:09:38 +0700

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,78 @@
 @extends('layouts.login')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+<section class="login p-fixed d-flex text-center bg-success">
+    <!-- Container-fluid starts -->
+    <div class="container-fluid">
+        <div class="row">
+
+            <div class="col-sm-12">
+                <div class="login-card card-block">
+                    <form class="md-float-material" method="POST" action="{{ route('login') }}">
                         @csrf
-
-                        <div class="form-group row">
-                            <label for="username" class="col-sm-4 col-form-label text-md-right">{{ __('Username') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" required autofocus>
+                        <div class="text-center">
+                            <img src="{{ asset('assets_frontend/images/logo.png') }}" alt="logo">
+                        </div>
+                        {{-- <h3 class="text-center txt-success">
+                            Sign In to your account
+                        </h3> --}}
+                        <div class="md-input-wrapper">
+                            <input id="username" type="text" class="md-form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" required autofocus>
 
                                 @if ($errors->has('username'))
                                     <span class="invalid-feedback">
                                         <strong>{{ $errors->first('username') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                        <div class="md-input-wrapper">
+                            <input id="password" type="password" class="md-form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required placeholder="Username">
 
                                 @if ($errors->has('password'))
                                     <span class="invalid-feedback">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
+                        <div class="row">
+                            <div class="col-sm-6 col-xs-12">
+                            <div class="rkmd-checkbox checkbox-rotate checkbox-ripple m-b-25">
+                                <label class="input-checkbox checkbox-success">
+                                    <input type="checkbox" id="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }} placeholder="Password">
+                                    <span class="checkbox"></span>
+                                </label>
+                                <div class="captions">Remember Me</div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
-                                    </label>
+                            </div>
                                 </div>
+                            <div class="col-sm-6 col-xs-12 forgot-phone text-right">
+                                <a href="{{ route('password.request') }}" class="text-right f-w-600"> Forget Password?</a>
+                                </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-10 offset-xs-1">
+                                <button type="submit" class="btn btn-success btn-md btn-block waves-effect text-center m-b-20">LOGIN</button>
                             </div>
                         </div>
+                        <!-- <div class="card-footer"> -->
+                        {{-- <div class="col-sm-12 col-xs-12 text-center">
+                            <span class="text-muted">Don't have an account?</span>
+                            <a href="register2.html" class="f-w-600 p-l-5">Sign up Now</a>
+                        </div> --}}
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            </div>
-                        </div>
+                        <!-- </div> -->
                     </form>
+                    <!-- end of form -->
                 </div>
+                <!-- end of login-card -->
             </div>
+            <!-- end of col-sm-12 -->
         </div>
+        <!-- end of row -->
     </div>
-</div>
+    <!-- end of container-fluid -->
+</section>
+
+
 @endsection
